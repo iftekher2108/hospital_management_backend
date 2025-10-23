48357acedf55c652997c84c8d6d6a0617cfcc514
iftekher2108 <parvezkhan21082002@gmail.com>
2025-10-23 15:19:22 +0600

    user cruds complete

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,5 +3,8 @@ const userController = require('../controllers/user.controllers')
 
 router.get('/', userController.getUsers);
 router.post('/', userController.createUser);
+router.get('/:id', userController.getUserById);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
