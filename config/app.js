aa0509c48e742f64eaa17db5b547469a46ea6f87
iftekher2108 <parvezkhan21082002@gmail.com>
2025-10-25 13:31:16 +0600

    paginate plugin set with populate and to json modified and all paginate set now need to config the Auth

--- config/app.js
+++ config/app.js
@@ -1,4 +1,9 @@
 require('dotenv').config()
 
+exports.JWT_SECRET = process.env.JWT_SECRET
+exports.JWT_EXPIRE = process.env.JWT_EXPIRE
+
 exports.PORT = process.env.PORT;
 exports.MONGODB_URI = process.env.MONGODB_URI;
+
+
